a03ba82c1281d7b4989c9d58a5ab65a8a9bc2294
Casper Nyaora <nyaora.casper@gmail.com>
2026-02-16 14:49:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2590362330-1bb15",
+  "appId": "1:148239918344:web:3f7897a5a5af03c9eff8bd",
+  "apiKey": "AIzaSyBHsyyJH79NvigGi9c362p2MGt6OXqoNa4",
+  "authDomain": "studio-2590362330-1bb15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "148239918344"
+};
